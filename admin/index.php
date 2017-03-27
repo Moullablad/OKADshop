fbc35f40720c31ed051908763a998c07906dddc1
dev2moullablad <dev2@moullablad.com>
2017-03-27 10:03:40 +0000

    Updating blog cms view

--- admin/index.php
+++ admin/index.php
@@ -16,6 +16,7 @@ if( $module === '' ) AdminController::getDashboard();
 if( isset($_GET['page']) && $_GET['page'] != '' ) {
 	PageController::render($_GET['page']);
 }
+
+
 require 'includes/controller.php';
 require 'includes/views.php';
-?>
