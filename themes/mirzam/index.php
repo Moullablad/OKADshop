47079921c74bd8a8070d0b95bd9b2d0c11d85001
Brahim Moullablad <softhightech@users.noreply.github.com>
2017-04-22 22:31:23 +0200

    i fixed some issues with left category responsive.

--- themes/mirzam/index.php
+++ themes/mirzam/index.php
@@ -1,7 +1,7 @@
 <section class="mt-20" id="home_category">
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
                 <?php get_section('column_left'); ?>
             </div>
             <div class="col-md-9 p-0">
